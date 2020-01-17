cda07bae7e45abb7f1663638ded7c418bf3114e5
sak <sak@sak.com>
2020-01-17 09:33:29 +0800

    添加文件夹

--- WpfApplicationTest1/MainWindow.xaml.cs
+++ WpfApplicationTest1/MainWindow.xaml.cs
@@ -24,10 +24,6 @@ namespace WpfApplicationTest1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string pattern = "^[0-9](.|.[0-9])?$";
-        private string lastScaleValue = "";
-
-
         public MainWindow()
         {
             InitializeComponent();
@@ -49,12 +45,16 @@ namespace WpfApplicationTest1
                     bi.UriSource = new Uri(openFileDialog.FileName, UriKind.Absolute);
                     bi.EndInit();
                     BackgroundImage.Source = bi;
+                    ScaleBackgroundImage(1);
                     TBoxImgPath.Text = openFileDialog.FileName;
                 }
             }
         }
 
-        private void TBoxScale_TextChanged_1(object sender, TextChangedEventArgs e)
+
+        private string lastScaleValue = "";
+        private const string patternTBoxScale = "^[0-9](.|.[0-9])?$";
+        private void TBoxScale_TextChanged(object sender, TextChangedEventArgs e)
         {
             string input = TBoxScale.Text;
             if (input == "")
@@ -62,19 +62,64 @@ namespace WpfApplicationTest1
                 lastScaleValue = "";
                 return;
             }
-            if (!Regex.IsMatch(input, pattern))
+            if (!Regex.IsMatch(input, patternTBoxScale))
             {
                 TBoxScale.Text = lastScaleValue;
                 TBoxScale.SelectionStart = lastScaleValue.Length;
                 return;
             }
+            // 保存上一个值
             lastScaleValue = input;
-
+            double dValue = Convert.ToDouble(lastScaleValue);
+            // 数字合理性判断
+            if (dValue > 10) dValue = 10;
+            else if (dValue <= 0) dValue = 1;
+            ScaleBackgroundImage(dValue);
         }
+
+        private string lastValueTBoxCell = "";
+        private const string patternTBoxCell = "^[0-9]{0,3}$";
+        private void TBoxCell_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string input = TBoxCell.Text;
+            if (input == "")
+            {
+                lastValueTBoxCell = "";
+                return;
+            }
+            if (!Regex.IsMatch(input, patternTBoxCell))
+            {
+                TBoxCell.Text = lastValueTBoxCell;
+                TBoxCell.SelectionStart = lastValueTBoxCell.Length;
+                return;
+            }
+            // 保存上一个值
+            lastValueTBoxCell = input;
+        }
+        
+        private int cellLen = 0;
+        private void Button_Click_Generate_Cell(object sender, RoutedEventArgs e)
+        {
+            if (lastValueTBoxCell == "") return;
+            int iValue = Convert.ToInt32(lastValueTBoxCell);
+            // 数字合理性判断
+            if (iValue > 100) iValue = 100;
+            else if (iValue <= 0) iValue = 1;
+            cellLen = iValue;
+            // 生成按钮
+            GenerateButton();
+        }  
 
         private void Print(string str)
         {
             Console.WriteLine(str);
         }
+
+        private void BackgroundImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ImageCanvas.Width = BackgroundImage.Width;
+            ImageCanvas.Height = BackgroundImage.Height;
+        }
+     
     }
 }
--- WpfApplicationTest1/ToolControls/ToolButton.cs
+++ WpfApplicationTest1/ToolControls/ToolButton.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfApplicationTest1.ToolControls
 {
     public class ToolButton : Button
     {
+        private readonly string borderColor = "#FF707070";
+        private readonly static ResourceDictionary rd = new ResourceDictionary();
+        private static ControlTemplate TP;
+
+        static ToolButton()
+        {
+            rd.Source = new Uri("ToolControls/ToolControls.xaml", UriKind.Relative);
+            TP = (ControlTemplate)rd["ToolButton"];
+        }
+
         public int Index { get; set; }
 
         public ToolButton()
         {
             base.Background = null;
+            base.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(borderColor));
+            base.Template = TP;
         }
 
         /// <summary>
